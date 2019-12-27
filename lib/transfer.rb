993ba3f73213805ac8c8efd83df862d2949ba0e0
Dimitri Gedda <gratitudeadore@fastmail.com>
2019-12-27 05:41:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,7 +6,8 @@ def initialize(sender, receiver, amount)
 end
 
 def valid?
-  
+  is_valid_transfer = false
+  sender.valid? == true && receiver.valid? == true ? is_valid_transfer = true : is_valid_transfer = false
 end
 
 def execute_transaction
