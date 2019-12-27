3b103c39be5eb109407fd3267679da3e504eb467
Dimitri Gedda <gratitudeadore@fastmail.com>
2019-12-27 05:26:42 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,12 +3,21 @@ class BankAccount
   attr_reader :name
   attraccessor :balance, :status
   
-  def initialize
-
-
+  def initialize(name)
+    @balance = 1000
+    @status = "open"
 end
 
 def deposit
+end
+
 def display_balance
+end
+
 def valid?
-  
+end
+
+def close_account
+end
+
+end
